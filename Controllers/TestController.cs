8d9790856f9271af2d5c859fdc6f51a07c7b9b3f
Johan Sundell <johansundell@gmail.com>
2018-09-15 16:20:32 +0000

    Playing with arrays

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
@@ -12,9 +13,15 @@ namespace TodoApi.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<TodoItem>> Get()
         {
-            return new string[] { "sudde1", "sudde2" };
+            var test = new TodoItem[] { new TodoItem{ nummer = 1, text = "test"} };
+            var t = test.Append( new TodoItem{nummer = 2, text ="test2" });
+            var l = t.ToList();
+            l.Add( new TodoItem{ nummer = 3, text = "test3"});
+            //test = test.Concat( new [] { new { nummer= 2, text = "test2"}});
+            //return new string[] { "sudde1", "sudde2" };
+            return l.ToArray();
         }
     }
 }
---
+++ models/TodoItem.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public string text{ get; set; }
+        public int nummer{ get; set; }
+    }
+}
