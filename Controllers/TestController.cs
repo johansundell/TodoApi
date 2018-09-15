86953c8d41785c8d8bd767dfcc4777d29af4b2a4
Johan Sundell <johansundell@gmail.com>
2018-09-15 16:43:05 +0000

    More testing

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -11,14 +11,18 @@ namespace TodoApi.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
-        // GET api/values
+        // GET api/test
         [HttpGet]
         public ActionResult<IEnumerable<TodoItem>> Get()
         {
             var test = new TodoItem[] { new TodoItem{ nummer = 1, text = "test"} };
             var t = test.Append( new TodoItem{nummer = 2, text ="test2" });
             var l = t.ToList();
+            var i = new TodoItem();
+            i.nummer = 4;
+            i.text = "test4";
             l.Add( new TodoItem{ nummer = 3, text = "test3"});
+            l.Add( i );
             //test = test.Concat( new [] { new { nummer= 2, text = "test2"}});
             //return new string[] { "sudde1", "sudde2" };
             return l.ToArray();
